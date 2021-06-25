bc02b0f0cde7c05abb87c8ab2abbd084a6ecabd5
villegas545 <villegas_545@hotmail.com>
2021-06-24 21:16:34 -0600

    commit

---
+++ Assets/script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void loadScene(string escena){
+        SceneManager.LoadScene(escena);
+        Debug.Log("hi");
+    }
+}
